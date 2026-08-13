679b9a4523dc13a3822f58a5295b1247a01eefdc
harithaemi <harithamanoj1969@gmail.com>
2026-08-13 23:26:28 +0530

    Fix Gmail SMTP configuration

--- src/middleware/sendEmail.js
+++ src/middleware/sendEmail.js
@@ -1,20 +1,24 @@
-const nodemailer=require("nodemailer");
+const nodemailer = require("nodemailer");
 
-const sendEmail=async(options)=>{
+const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        service: process.env.SMTP_SERVICE,
-        auth:{
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
             user: process.env.SMTP_MAIL,
-            pass: process.env.SMTP_PASSWORD
+            pass: process.env.SMTP_PASSWORD,
         },
     });
-    const mailOptions={
-        from:process.env.SMTP_MAIL,
-        to:options.email,
-        subject:options.subject,
-        text:options.message,
 
+    const mailOptions = {
+        from: process.env.SMTP_MAIL,
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
     };
-    await transporter.sendMail(mailOptions)
-}
-module.exports=sendEmail
+
+    await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
