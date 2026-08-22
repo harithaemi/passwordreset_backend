653263e960b4a8ff4b86c6a1bb121527fc58566c
harithaemi <harithamanoj1969@gmail.com>
2026-08-22 19:25:48 +0530

    final resend update

--- src/middleware/sendEmail.js
+++ src/middleware/sendEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (options) => {
     const { data, error } = await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "noreply@harithamanoj.in",
         to: [options.email],
         subject: options.subject,
         text: options.message,
